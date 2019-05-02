09b202674c3900f02e4a4d4c0f66d9812aef93c3
Annouar <alaifa@ebia.fr>
2019-05-02 20:10:21 +0200

    Add Auth to Pegass class

--- pegass_authentication.py
+++ pegass_authentication.py
@@ -2,40 +2,44 @@ import os
 
 import mechanicalsoup
 
-PEGASS_URL = 'https://pegass.croix-rouge.fr/'
-url_authentication = 'https://id.authentification.croix-rouge.fr'
-
-def login():
-  browser = mechanicalsoup.StatefulBrowser(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0')
-  browser.set_verbose(True)
-  browser.open(url_authentication)
-  print(browser.get_url())
-  browser.select_form('form[action="/my.policy"]')
-  browser['username'] = os.environ.get('LOGIN')
-  browser['password'] = os.environ.get('PASSWORD')
-  browser['vhost'] = 'standard'
-
-  browser.submit_selected()
-  browser.open('https://pegass.croix-rouge.fr/crf/rest')
-  saml_response = browser.get_current_page().findAll('input', {'name': 'SAMLResponse'})[0].get('value')
-  relay = browser.get_current_page().findAll('input', {'name': 'RelayState'})[0].get('value')
-
-  browser.select_form('form[action="https://pegass.croix-rouge.fr/Shibboleth.sso/SAML2/POST"]')
-  browser.submit_selected()
-
-  cookies = browser.get_cookiejar().get_dict()
-  print(cookies)
-
-  return cookies
-
-def request(url, cookies=None):
-  browser = mechanicalsoup.StatefulBrowser(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0')
-  auth_cookies = cookies
-  if cookies is None :  
-    auth_cookies = login()
-  return browser.get(url, cookies=auth_cookies).json()
-
-print(request('https://pegass.croix-rouge.fr/crf/rest/gestiondesdroits'))
+PEGASS_URL = 'https://pegass.croix-rouge.fr'
+URL_AUTHENTICATION = 'https://id.authentification.croix-rouge.fr'
+DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0'
 
 
+class PegassClient:
+    def __init__(self, pegass_url=PEGASS_URL, url_authentication=URL_AUTHENTICATION, user_agent=DEFAULT_USER_AGENT):
+        self.pegass_url = pegass_url
+        self.url_authentication = url_authentication
+        self.user_agent = user_agent
 
+    def login(self, username, password):
+        browser = mechanicalsoup.StatefulBrowser(user_agent=self.user_agent)
+        browser.open(self.url_authentication)
+        browser.select_form('form[action="/my.policy"]')
+        browser['username'] = username
+        browser['password'] = password
+        browser['vhost'] = 'standard'
+
+        browser.submit_selected()
+        browser.open('https://pegass.croix-rouge.fr/crf/rest')
+
+        browser.select_form('form[action="{}/Shibboleth.sso/SAML2/POST"]'.format(self.pegass_url))
+        browser.submit_selected()
+
+        cookies = browser.get_cookiejar().get_dict()
+        return cookies
+
+    def request(self, url_to_call, cookies=None):
+        browser = mechanicalsoup.StatefulBrowser(user_agent=self.user_agent)
+        auth_cookies = cookies
+        if cookies is None:
+            auth_cookies = self.login()
+        return browser.get('{}/{}'.format(self.pegass_url, url_to_call), cookies=auth_cookies).json()
+
+
+if __name__ == '__main__':
+    client = PegassClient()
+    auth_cookies = client.login(os.environ.get('username'), os.environ.get('password'))
+    rules = client.request('crf/rest/gestiondesdroits', cookies=auth_cookies)
+    print(rules)

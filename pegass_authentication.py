2b6e85e8f8bd330687172115d12a0a45a69f6fab
Annouar <alaifa@ebia.fr>
2019-05-04 19:46:19 +0200

    Update base code to export functions instead of classes

--- pegass_authentication.py
+++ pegass_authentication.py
@@ -3,43 +3,39 @@ import os
 import mechanicalsoup
 
 PEGASS_URL = 'https://pegass.croix-rouge.fr'
-URL_AUTHENTICATION = 'https://id.authentification.croix-rouge.fr'
+AUTH_URL = 'https://id.authentification.croix-rouge.fr'
 DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0'
 
 
-class PegassClient:
-    def __init__(self, pegass_url=PEGASS_URL, url_authentication=URL_AUTHENTICATION, user_agent=DEFAULT_USER_AGENT):
-        self.pegass_url = pegass_url
-        self.url_authentication = url_authentication
-        self.user_agent = user_agent
+def login(username, password, pegass_url=PEGASS_URL, auth_url=AUTH_URL, user_agent=DEFAULT_USER_AGENT):
+    browser = mechanicalsoup.StatefulBrowser(user_agent=user_agent)
+    browser.open(auth_url)
+    browser.select_form('form[action="/my.policy"]')
+    browser['username'] = username
+    browser['password'] = password
+    browser['vhost'] = 'standard'
 
-    def login(self, username, password):
-        browser = mechanicalsoup.StatefulBrowser(user_agent=self.user_agent)
-        browser.open(self.url_authentication)
-        browser.select_form('form[action="/my.policy"]')
-        browser['username'] = username
-        browser['password'] = password
-        browser['vhost'] = 'standard'
+    browser.submit_selected()
+    browser.open('{}/crf/rest'.format(pegass_url))
 
-        browser.submit_selected()
-        browser.open('https://pegass.croix-rouge.fr/crf/rest')
+    browser.select_form('form[action="{}/Shibboleth.sso/SAML2/POST"]'.format(pegass_url))
+    browser.submit_selected()
 
-        browser.select_form('form[action="{}/Shibboleth.sso/SAML2/POST"]'.format(self.pegass_url))
-        browser.submit_selected()
+    cookies = browser.get_cookiejar().get_dict()
+    return cookies
 
-        cookies = browser.get_cookiejar().get_dict()
-        return cookies
 
-    def request(self, url_to_call, cookies=None):
-        browser = mechanicalsoup.StatefulBrowser(user_agent=self.user_agent)
-        auth_cookies = cookies
-        if cookies is None:
-            auth_cookies = self.login()
-        return browser.get('{}/{}'.format(self.pegass_url, url_to_call), cookies=auth_cookies).json()
+def request(url_to_call, pegass_url=PEGASS_URL, auth_url=AUTH_URL, user_agent=DEFAULT_USER_AGENT, **kwargs):
+    browser = mechanicalsoup.StatefulBrowser(user_agent=user_agent)
+    if 'cookies' in kwargs:
+        return browser.get('{}/{}'.format(pegass_url, url_to_call), cookies=kwargs['cookies']).json()
+    if 'username' in kwargs and 'password' in kwargs:
+        auth_cookies = login(kwargs['username'], kwargs['password'], pegass_url, auth_url, user_agent)
+        return browser.get('{}/{}'.format(pegass_url, url_to_call), cookies=auth_cookies).json()
+    raise TypeError('Missing either cookies or username/password to achieve the request')
 
 
 if __name__ == '__main__':
-    client = PegassClient()
-    auth_cookies = client.login(os.environ.get('username'), os.environ.get('password'))
-    rules = client.request('crf/rest/gestiondesdroits', cookies=auth_cookies)
+    auth_cookies = login(os.environ.get('username'), os.environ.get('password'))
+    rules = request('crf/rest/gestiondesdroits', cookies=auth_cookies)
     print(rules)
